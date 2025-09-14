b69c342915a5a31423abb70ae487fad9d5931d12
Davide Palma (ElDavo) <github@davidepalma.it>
2025-09-14 10:24:32 +0200

    disable intel compute runtime

--- configuration.nix
+++ configuration.nix
@@ -348,7 +348,7 @@
     enable = true;
     extraPackages = with pkgs; [
       #... # your Open GL, Vulkan and VAAPI drivers
-      intel-compute-runtime
+      #intel-compute-runtime
       intel-media-driver
       vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
       # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
